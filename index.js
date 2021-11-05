f32c718fbfced1e50e2880b0a30a1eb679e45473
Dawid P <dawid.pomiotlo@gmail.com>
2021-11-05 20:08:58 +0100

    add confing for db adress and port number

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+const mongoose = require('mongoose');
+const config = require('config');
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
 const notes = require('./routes/notes');
 const user = require('./routes/user');
 const auth = require('./routes/auth');
 
 
 //connect to db
-const dbUrl = "mongodb://localhost:27017/notes";
+const dbUrl = config.get('db');
 mongoose.connect(dbUrl).then(()=> console.log("connected to db")).catch(console.log);
 
 //middleware
@@ -18,4 +19,5 @@ app.use('/api/notes', notes);
 app.use('/api/user', user);
 app.use('/api/auth', auth);
 
-app.listen(3001, ()=>console.log("listen on port 3001..."))
+const port = config.get('port');
+app.listen(port, ()=> console.log(`listen on port ${port}...`));
--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcrypt');
 const _ = require('lodash');
 const {User} = require("../models/user");
 
-// add: config
+
 router.post('/', async (req, res)=>{
     const {error} = validateAuth(req.body);
     if(error) return res.status(400).send(error.details[0].message);
